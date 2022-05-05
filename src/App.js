46c9f7544539ee3bab9c6e394f127542f1b682cb
Mahfod <mahfod.dev@gmail.com>
2022-05-05 21:38:44 +0200

    change router by 6

--- src/App.js
+++ src/App.js
@@ -20,11 +20,20 @@ function App() {
 			<Router>
 				<Navbar />
 				<Sidebar />
-				
+
 				<Routes>
-					
+					<Route path='/' element={<Home />} />
+					<Route path='about' element={<About />} />
+					<Route path='cart' element={<Cart />} />
+					<Route path='products' element={<Products />} />
+					<Route path='products/:id' element={<SingleProduct />} />
+					<Route
+						path='checkout'
+						element={<PrivateRoute>{<Checkout />}</PrivateRoute>}
+					/>
+					<Route path='*' element={<Error />} />
 				</Routes>
-				
+
 				<Footer />
 			</Router>
 		</AuthWrapper>
